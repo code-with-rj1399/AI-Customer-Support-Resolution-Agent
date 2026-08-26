8ddccb52c11359eff64f272fbdae26decb76e709
CodeWithRJ <13061978+code-with-rj1399@users.noreply.github.com>
2026-08-26 19:20:55 +0530

    feat: add application clock configuration

---
+++ src/main/java/com/rj1399/customersupport/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.rj1399.customersupport.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.time.Clock;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    Clock clock() {
+        return Clock.systemUTC();
+    }
+}
